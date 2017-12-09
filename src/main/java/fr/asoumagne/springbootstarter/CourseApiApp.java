c308a36ad8830aaed51644b51d03c2ea9fd88ccb
unknown <alain21218@gmail.com>
2017-12-09 14:50:07 +0100

    fin

--- src/main/java/fr/asoumagne/springbootstarter/CourseApiApp.java
+++ src/main/java/fr/asoumagne/springbootstarter/CourseApiApp.java
@@ -1,12 +1,20 @@
 package fr.asoumagne.springbootstarter;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class CourseApiApp {
 
-	public static void main(String[] args) {
+	@RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+	
+	public static void main(String[] args) throws Exception {
 		
 		SpringApplication.run(CourseApiApp.class, args);
 	}
